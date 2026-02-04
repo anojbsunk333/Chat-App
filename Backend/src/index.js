f1be9d9b5dfc2a8abd08674b610425cc1453f8d6
anojbsunk333 <anojbsunk500@gmail.com>
2026-02-04 10:41:39 +0545

    restart

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,31 +1,31 @@
-
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
 import messageRoutes from "./routes/message.routes.js";
-import  authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
 import { app, server } from "./lib/socket.js";
-
-
 
 dotenv.config();
 
-
-
 const PORT = process.env.Port || 5000;
 
-app.use(express.json({ limit: '50mb'}));
-app.use(express.urlencoded({ extended: true, limit: "50mb" })); 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-})
+app.use(
+  cors({
+    origin: [
+      "http://192.168.112.119:3000",
+      "http://localhost:3000",
+      "https://chat-app-three-pi-78.vercel.app",
+    ],
+    credentials: true,
+  }),
 );
 
 app.use("/api/auth", authRoutes);
@@ -43,4 +43,3 @@ server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
 });
-
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,4 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    host: true, // or '0.0.0.0' to bind to all interfaces
+  },
 });
