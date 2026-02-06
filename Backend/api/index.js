4f500fbdb397db4f4946228b90f468429c785390
anojbsunk333 <anojbsunk500@gmail.com>
2026-02-06 16:31:48 +0545

    Fix: Properly export Express app for Vercel serverless environment

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,6 +1,3 @@
 import app from "../src/index.js";
 
-export default async (req, res) => {
-  // Ensure routes are properly mounted in serverless environment
-  return app(req, res);
-};
+export default app;
